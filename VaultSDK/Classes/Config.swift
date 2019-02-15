47dfd5f454e537a765e31e677fabec6f178b55a0
Alex Huang <alexhuangtung@gmail.com>
2019-02-15 15:22:31 +0800

    Define Config object to parse Info.plist

---
+++ VaultSDK/Classes/Config.swift
@@ -0,0 +1,21 @@
+//
+//  Config.swift
+//  AppAuth
+//
+//  Created by Alex Huang on 2019/2/15.
+//
+
+import Foundation
+
+struct Config: Decodable {
+    
+    let clientId: String
+    
+    let clientSecret: String
+    
+    enum CodingKeys: String, CodingKey {
+        case clientId = "ClientId"
+        case clientSecret = "ClientSecret"
+    }
+    
+}
