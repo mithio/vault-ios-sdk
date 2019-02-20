025e6fbb52a928a6094c7b0291a89b289cd3665a
Alex Huang <alexhuangtung@gmail.com>
2019-02-20 14:31:58 +0800

    Add UI for swift example app

--- Examples/Example_Swift/Example_Swift/ViewController.swift
+++ Examples/Example_Swift/Example_Swift/ViewController.swift
@@ -11,53 +11,129 @@ import VaultSDK
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        if VaultSDK.shared.isLoggedIn {
-            VaultSDK.shared.getUserInformation { (userInfo, error) in
-                guard let userInfo = userInfo else {
-                    NSLog("\(error)")
-                    return
-                }
-                
-                NSLog("\(userInfo)")
-            }
-            
-            VaultSDK.shared.getClientInformation { (balances, error) in
-                guard let balances = balances else {
-                    NSLog("\(error)")
-                    return
-                }
-                
-                NSLog("\(balances)")
-            }
-            
-            VaultSDK.shared.getUserMiningAction { (miningActivities, error) in
-                guard let miningActivities = miningActivities else {
-                    NSLog("\(error)")
-                    return
-                }
-                
-                NSLog("\(miningActivities)")
-            }
-            
-            VaultSDK.shared.postUserMiningAction(reward: 123.789, uuid: UUID().uuidString) { (success, error) in
-                NSLog("\(success)")
-            }
-        } else {
-            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
-                VaultSDK.shared.login(from: self) { (token, error) in
-                    guard let token = token else {
-                        NSLog("\(error)")
-                        return
-                    }
-                    
-                    NSLog(token)
-                }                
-            }
+    @IBOutlet weak var loginWithVaultButton: UIButton!
+    @IBOutlet weak var getUserInfoButton: UIButton!
+    @IBOutlet weak var getClientInfoButton: UIButton!
+    @IBOutlet weak var getUserMiningActionButton: UIButton!
+    @IBOutlet weak var postUserMiningActionButton: UIButton!
+    @IBOutlet weak var rewardTextField: UITextField! {
+        didSet {
+            rewardTextField.placeholder = "0.12345678"
+            rewardTextField.keyboardType = .decimalPad
         }
     }
+    @IBOutlet weak var unbindVaultButton: UIButton!
+    @IBOutlet weak var consoleTextView: UITextView! {
+        didSet {
+            if VaultSDK.shared.isLoggedIn {
+                consoleTextView.text = "User is already logged in with access token, \(VaultSDK.shared.accessToken!).\n\n"
+            } else {
+                consoleTextView.text = "Please log in with VAULT.\n\n"
+            }
+            consoleTextView.isEditable = false
+            consoleTextView.layer.borderWidth = 0.5
+            consoleTextView.layer.cornerRadius = 8
+            consoleTextView.layer.borderColor = UIColor.black.cgColor
+            consoleTextView.keyboardDismissMode = .interactive
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    private func updateUI() {
+        let isLoggedIn = VaultSDK.shared.isLoggedIn
+        loginWithVaultButton.isEnabled = !isLoggedIn
+        getUserInfoButton.isEnabled = isLoggedIn
+        getClientInfoButton.isEnabled = isLoggedIn
+        getUserMiningActionButton.isEnabled = isLoggedIn
+        postUserMiningActionButton.isEnabled = isLoggedIn
+        rewardTextField.isEnabled = isLoggedIn
+        unbindVaultButton.isEnabled = isLoggedIn
+    }
 
-
+    @IBAction func loginWithVault(_ sender: UIButton) {
+        VaultSDK.shared.login(from: self) { (token, error) in
+            guard let token = token else {
+                self.consoleTextView.text += "\(error!.localizedDescription)\n\n"
+                return
+            }
+            
+            self.updateUI()
+            self.consoleTextView.text += "Successfully logged in, got access token, \(token).\n\n"
+        }
+    }
+    
+    @IBAction func getUserInfo(_ sender: UIButton) {
+        VaultSDK.shared.getUserInformation { (userInfo, error) in
+            guard let userInfo = userInfo else {
+                self.consoleTextView.text += "\(error!.localizedDescription)\n\n"
+                return
+            }
+            
+            self.consoleTextView.text += "User information: \(userInfo)\n\n"
+        }
+    }
+    
+    @IBAction func getClinetInfo(_ sender: UIButton) {
+        VaultSDK.shared.getClientInformation { (balances, error) in
+            guard let balances = balances else {
+                self.consoleTextView.text += "\(error!.localizedDescription)\n\n"
+                return
+            }
+            
+            self.consoleTextView.text += "Client information: \(balances)\n\n"
+        }
+    }
+    
+    @IBAction func getUserMingingAction(_ sender: UIButton) {
+        VaultSDK.shared.getUserMiningAction { (miningActivities, error) in
+            guard let miningActivities = miningActivities else {
+                self.consoleTextView.text += "\(error!.localizedDescription)\n\n"
+                return
+            }
+            
+            self.consoleTextView.text += "Mining activities: \(miningActivities)\n\n"
+        }
+    }
+    
+    @IBAction func postUserMiningAction(_ sender: UIButton) {
+        guard let reward = Double(rewardTextField.text ?? "") else {
+            self.consoleTextView.text += "Please fill a number in the reward text field.\n\n"
+            return
+        }
+        
+        VaultSDK.shared.postUserMiningAction(reward: reward, uuid: UUID().uuidString) { (success, error) in
+            guard success else {
+                self.consoleTextView.text += "\(error!.localizedDescription)\n\n"
+                return
+            }
+            
+            self.rewardTextField.resignFirstResponder()
+            self.rewardTextField.text = nil
+            self.consoleTextView.text += "Successfully post user mining action with reward, \(reward).\n\n"
+        }
+        
+    }
+    
+    @IBAction func unbindVault(_ sender: UIButton) {
+        let accessToken = VaultSDK.shared.accessToken!
+        VaultSDK.shared.unbind { (success, error) in
+            guard success else {
+                self.consoleTextView.text += "\(error!.localizedDescription)\n\n"
+                return
+            }
+            
+            self.updateUI()
+            self.consoleTextView.text += "Successfully unbind access token, \(accessToken).\n\n"
+        }
+    }
+    
+    @IBAction func clearConsole(_ sender: UIButton) {
+        consoleTextView.text = ""
+    }
+    
 }
 
